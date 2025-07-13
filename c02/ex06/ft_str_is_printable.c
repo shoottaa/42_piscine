62ff633231290527335d5fb8c03a184d988e654e
Erwan Gaudicheau <ton.email@exemple.com>
2025-07-13 17:31:33 +0200

    ex05 06

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egaudich <egaudich@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/13 17:26:05 by egaudich          #+#    #+#             */
-/*   Updated: 2025/07/13 17:26:05 by egaudich         ###   ########.fr       */
+/*   Created: 2025/07/13 17:26:01 by egaudich          #+#    #+#             */
+/*   Updated: 2025/07/13 17:26:01 by egaudich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdio.h>
 
-int ft_str_is_uppercase(char *str)
+int ft_str_is_printable(char *str)
 {
     int i;
-
     i = 0;
     while (str[i])
     {
-        if (!(str[i] >= 'A' && str[i] <= 'Z'))
+        if (!(str[i] >= 32 && str[i] <= 126))
             return (0);
         i++;
     }
@@ -28,7 +27,7 @@ int ft_str_is_uppercase(char *str)
 
 // int main(void)
 // {
-//     int test = ft_str_is_uppercase("test123");
+//     int test = ft_str_is_printable("test");
 //     printf("%i", test);
 //     return (0);
 // }
