c7ede6405363d337969d684c843509a91410dfea
Erwan Gaudicheau <ton.email@exemple.com>
2025-07-16 12:09:55 +0200

    C03 et C04

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -21,22 +21,20 @@ void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
-		// Cas spécial, on l'écrit directement
 		write(1, "-2147483648", 11);
-		return;
+		return ;
 	}
 	if (nb < 0)
 	{
-		ft_putchar('-');         // Dire "moins"
-		nb = -nb;                // On le rend positif
+		ft_putchar('-');
+		nb = -nb;
 	}
 	if (nb >= 10)
 	{
-		ft_putnbr(nb / 10);      // On dit les chiffres avant
+		ft_putnbr(nb / 10);
 	}
-	ft_putchar(nb % 10 + '0');  // On dit le dernier chiffre
+	ft_putchar(nb % 10 + '0');
 }
-
 
 // int	main(void)
 // {
