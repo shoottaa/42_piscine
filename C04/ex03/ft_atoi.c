58b789643b013f89795f40476e88c749956db197
Erwan Gaudicheau <ton.email@exemple.com>
2025-07-16 19:07:41 +0200

    c04

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egaudich <egaudich@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 18:36:57 by egaudich          #+#    #+#             */
+/*   Updated: 2025/07/16 18:36:57 by egaudich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int sign;
+	int nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' 
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (sign * nb);
+}
+
+int main()
+{
+    printf("%d\n", ft_atoi(" ---+--+1234ab567"));  // Résultat : -123
+}
